8e433e267bb0ffee6b237b950835bd00d4c1bd2d
Alexandre <varellalexandre@unp.edu.br>
2019-03-30 14:51:22 -0300

    Operators overloaded

--- Cash.py
+++ Cash.py
@@ -8,29 +8,46 @@ class CashFlow:
 			self.cashflow.append(int(j))
 
 	def __add__(self,o):
+		aux = []
 		if (len(self.cashflow)) < (len(o.cashflow)):
 			diference = (len(o.cashflow))-(len(self.cashflow))
 			for i in range(diference):
 				self.cashflow.append(0)
-
+			aux = o.cashflow
+		elif len(self.cashflow) > len(o.cashflow):
+			diference = (len(self.cashflow))-(len(o.cashflow))
+			for i in o.cashflow:
+				aux.append(int(i))
+			for i in range(diference):
+				aux.append(0)
 		for i in range(len(self.cashflow)):
-			self.cashflow[i] = self.cashflow[i]+o.cashflow[i]
+			aux[i] = self.cashflow[i]+aux[i]
+		return CashFlow(cashflow=aux)
 
 	def __sub__(self,o):
+		aux = []
 		if (len(self.cashflow)) < (len(o.cashflow)):
 			diference = (len(o.cashflow))-(len(self.cashflow))
 			for i in range(diference):
 				self.cashflow.append(0)
-
+			aux = o.cashflow
+		elif len(self.cashflow) > len(o.cashflow):
+			diference = (len(self.cashflow))-(len(o.cashflow))
+			for i in o.cashflow:
+				aux.append(int(i))
+			for i in range(diference):
+				aux.append(0)
 		for i in range(len(self.cashflow)):
-			self.cashflow[i] = self.cashflow[i]-o.cashflow[i]
+			aux[i] = self.cashflow[i]-aux[i]
+		return CashFlow(cashflow=aux)
 
 	def toPresent(self,tax):
 		present_value = 0
+		cm = CashModify()
 		for i in range(len(self.cashflow)):
-			present_value = present_value + self.cashflow[i]*CashModify.transform('P/F',tax,i+self.start)
+			present_value = present_value + self.cashflow[i]*cm.transform(tax,i,'P/F')
 		return present_value
-		
+
 	def toFuture(self,tax,periods=1):
 		pass
 
@@ -39,26 +56,27 @@ class CashFlow:
 
 
 class CashModify:
-	def transform(self,conversion='F/P',tax,periods):
-		i = 0
-		if conversion=='P/A' or conversion=='P/U':
-			return self.PGA(tax,periods)
-		elif conversion=='A/P' or conversion=='U/P':
-			return (1/self.PGA(tax,periods))
-		elif conversion=='F/P':
-			return self.PGF(tax,periods)
-		elif conversion=='P/F':
-			return (1/self.PGF(tax,periods))
-		elif conversion=='F/A' or conversion=='F/U':
-			return self.PGA(tax,periods)*self.PGF(tax,periods)
-		elif conversion=='A/F' or conversion=='U/F':
-			return (1/self.PGA(tax,periods))*(1/self.PGF(tax,periods))
-
-	def PGA(self,tax,periods):
+	def PGivenA(self,tax,periods):
 		return (((1+tax)**periods)-1)/(((1+tax)**periods)*tax)
 
-	def PGF(self,tax,periods):
+	def PGivenF(self,tax,periods):
 		return (1+tax)**periods
+
+	def transform(self,tax,periods,conversion='F/P'):
+		i = 0
+		if conversion=='P/A' or conversion=='P/U':
+			return self.PGivenA(tax,periods)
+		elif conversion=='A/P' or conversion=='U/P':
+			return (1/self.PGivenA(tax,periods))
+		elif conversion=='F/P':
+			return self.PGivenF(tax,periods)
+		elif conversion=='P/F':
+			return (1/self.PGivenF(tax,periods))
+		elif conversion=='F/A' or conversion=='F/U':
+			return self.PGivenA(tax,periods)*self.PGivenF(tax,periods)
+		elif conversion=='A/F' or conversion=='U/F':
+			return (1/self.PGivenA(tax,periods))*(1/self.PGivenF(tax,periods))
+
 
 
 
--- Finance.py
+++ Finance.py
@@ -1,1 +1,12 @@
 from Cash import *
+
+if __name__=='__main__':
+	a = [2,3,4,5,1,34,12,41,2,3,123,2,3,3,3,3,3,3,3,3]
+	b = [3,2,1,4]
+	ca = CashFlow(cashflow=a)
+	cb = CashFlow(cashflow=b,start=3)
+	cc = ca+cb
+	print(ca.cashflow)
+	print(cb.cashflow)
+	print(cc.cashflow)
+	print(cb.toPresent(0.1))
